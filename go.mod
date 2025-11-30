517424dfbb4cc9197c6a7e890753384ca4068d67
Tochemey <tochemey@hotmail.com>
2025-11-30 00:40:44 +0000

    feat: :sparkles: add opentelemetry tracing

--- go.mod
+++ go.mod
@@ -1,23 +1,31 @@
 module github.com/groupcache/groupcache-go/v3
 
-go 1.21.7
-
-toolchain go1.23.1
+go 1.24.0
 
 require (
-	github.com/kapetan-io/tackle v0.10.0
-	github.com/maypok86/otter v1.2.0
+	github.com/kapetan-io/tackle v0.13.0
+	github.com/maypok86/otter v1.2.4
 	github.com/segmentio/fasthash v1.0.3
-	github.com/stretchr/testify v1.9.0
-	golang.org/x/net v0.22.0
-	google.golang.org/protobuf v1.28.1
+	github.com/stretchr/testify v1.11.1
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.63.0
+	go.opentelemetry.io/otel v1.38.0
+	go.opentelemetry.io/otel/sdk v1.38.0
+	go.opentelemetry.io/otel/trace v1.38.0
+	golang.org/x/net v0.47.0
+	google.golang.org/protobuf v1.36.10
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dolthub/maphash v0.1.0 // indirect
-	github.com/gammazero/deque v0.2.1 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/gammazero/deque v1.2.0 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/xerrors v0.0.0-20220609144429-65e65417b02f // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/otel/metric v1.38.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ transport/tracer_test.go
@@ -0,0 +1,129 @@
+/*
+Copyright Derrick J Wippler
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package transport
+
+import (
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
+)
+
+type recorderTracerProvider struct {
+	trace.TracerProvider
+	mu        sync.Mutex
+	requested []string
+}
+
+func (r *recorderTracerProvider) Tracer(name string, opts ...trace.TracerOption) trace.Tracer {
+	r.mu.Lock()
+	r.requested = append(r.requested, name)
+	r.mu.Unlock()
+	return r.TracerProvider.Tracer(name, opts...)
+}
+
+func newRecorderTracerProvider() *recorderTracerProvider {
+	return &recorderTracerProvider{TracerProvider: trace.NewNoopTracerProvider()}
+}
+
+func TestNewTracerUsesGlobalProviderWhenNoneProvided(t *testing.T) {
+	t.Parallel()
+
+	original := otel.GetTracerProvider()
+	rec := newRecorderTracerProvider()
+	otel.SetTracerProvider(rec)
+	defer otel.SetTracerProvider(original)
+
+	tracer := NewTracer()
+
+	require.Equal(t, rec, tracer.traceProvider)
+	require.Equal(t, []string{instrumentationName}, rec.requested)
+	require.NotNil(t, tracer.getTracer())
+}
+
+func TestNewTracerRespectsCustomProviderOption(t *testing.T) {
+	t.Parallel()
+
+	original := otel.GetTracerProvider()
+	defer otel.SetTracerProvider(original)
+
+	// Set a global provider that should not be used after the override.
+	global := newRecorderTracerProvider()
+	otel.SetTracerProvider(global)
+
+	custom := newRecorderTracerProvider()
+	tracer := NewTracer(WithTraceProvider(custom))
+
+	require.Equal(t, custom, tracer.traceProvider)
+	require.Equal(t, []string{instrumentationName}, custom.requested)
+	require.Empty(t, global.requested)
+}
+
+func TestWithTracerAttributesAppendsAttributes(t *testing.T) {
+	t.Parallel()
+
+	attrs := []attribute.KeyValue{
+		attribute.String("env", "test"),
+		attribute.Int("shard", 1),
+	}
+
+	tracer := NewTracer(WithTracerAttributes(attrs...))
+	require.Equal(t, attrs, tracer.traceAttributes)
+}
+
+func TestSpanStartOptionsPoolResetsOnPut(t *testing.T) {
+	t.Parallel()
+
+	tracer := NewTracer()
+
+	opts := tracer.getSpanStartOptions()
+	require.Zero(t, len(*opts))
+	require.GreaterOrEqual(t, cap(*opts), 10)
+
+	*opts = append(*opts, trace.WithSpanKind(trace.SpanKindClient))
+	require.Equal(t, 1, len(*opts))
+
+	tracer.putSpanStartOptions(opts)
+
+	opts = tracer.getSpanStartOptions()
+	require.Zero(t, len(*opts))
+	require.GreaterOrEqual(t, cap(*opts), 10)
+	tracer.putSpanStartOptions(opts)
+}
+
+func TestAttributesPoolResetsOnPut(t *testing.T) {
+	t.Parallel()
+
+	tracer := NewTracer()
+
+	attrs := tracer.getAttributes()
+	require.Zero(t, len(*attrs))
+	require.GreaterOrEqual(t, cap(*attrs), 10)
+
+	*attrs = append(*attrs, attribute.String("key", "val"))
+	require.Equal(t, 1, len(*attrs))
+
+	tracer.putAttributes(attrs)
+
+	attrs = tracer.getAttributes()
+	require.Zero(t, len(*attrs))
+	require.GreaterOrEqual(t, cap(*attrs), 10)
+	tracer.putAttributes(attrs)
+}
